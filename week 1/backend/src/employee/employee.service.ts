1d46f9eb571035fafd92486463f000fc9773cb9d
AwaisHamadAshraf <awaishappy021@gmail.com>
2026-01-09 12:42:46 +0500

    services file changed

--- week 1/backend/src/employee/employee.service.ts
+++ week 1/backend/src/employee/employee.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class EmployeeService {
     getEmployee(){
-        return 'hello from Services'
+        return 'hello from Servicesss'
     }
 }
