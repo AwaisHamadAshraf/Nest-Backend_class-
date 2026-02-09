3bc6bb6768b6b5e329cd3cddbe320a870dd4e702
AwaisHamadAshraf <awaishappy021@gmail.com>
2026-02-09 15:47:44 +0500

    fixex and additional files deleted

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
-import { EmployeeModule } from './employee/employee.module';
-import { TodoModule } from './todo/todo.module';
 import { StudentModule } from './student/student.module';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
-    EmployeeModule,
-    TodoModule,
     StudentModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/todo/todo.entity.ts
+++
@@ -1,5 +0,0 @@
-host: 'localhost'
-port: 5432
-username: 'postgres'
-password: 'Happy2002'
-database: 'Test_database'
